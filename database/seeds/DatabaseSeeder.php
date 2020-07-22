584e47b72d0cd84e1010b839403bd71428254870
Trio s <triosaputra308@gmail.com>
2020-07-23 00:03:07 +0700

    uncomment seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,8 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call('UsersTableSeeder');
-        // $this->call('AuthorSeeder');
-        // $this->call('PostSeeder');
+        $this->call('AuthorSeeder');
+        $this->call('PostSeeder');
         $this->call('CommentSeeder');
     }
 }
